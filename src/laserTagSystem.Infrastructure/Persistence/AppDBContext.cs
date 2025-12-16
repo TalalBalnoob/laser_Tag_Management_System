bc399c72a3190969cd96a7a47f492bdf37ddd598
TalalBalnoob <TalalBalnoob@gmail.com>
2025-12-16 13:58:01 +0300

    feat: add the Field to the context

--- src/laserTagSystem.Infrastructure/Persistence/AppDBContext.cs
+++ src/laserTagSystem.Infrastructure/Persistence/AppDBContext.cs
@@ -10,4 +10,5 @@ public class AppDBContext: DbContext  {
     
     public DbSet<Player> Players { get; set; }
     public DbSet<Team> Teams { get; set; }
+    public DbSet<Field> Fields { get; set; }
 }
