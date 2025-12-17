3390f6d1a0293d2301ab062fcbbc29e8af929389
TalalBalnoob <TalalBalnoob@gmail.com>
2025-12-17 10:43:41 +0300

    fix: add some annotations and missing relation

--- src/laserTagSystem.Domain/Entity/Field.cs
+++ src/laserTagSystem.Domain/Entity/Field.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace laserTagSystem.Domain.Entity;
 
 public class Field {
+    [Key]
     public Guid Id { get; set; }
     public string Name { get; set; }
+    
+    [Range(0,999)]
     public int Capacity { get; set; }
     public bool IsAvailable { get; set; } = true;
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public ICollection<Match> Matches { get; set; } = new List<Match>();
 }
