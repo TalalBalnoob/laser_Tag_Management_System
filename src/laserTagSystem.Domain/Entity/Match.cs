093cf807144bae8065db3bc62cbdf958b3d4fe5f
TalalBalnoob <TalalBalnoob@gmail.com>
2025-12-17 10:44:26 +0300

    fix: null relation to null! and add Key annotation

--- src/laserTagSystem.Domain/Entity/Match.cs
+++ src/laserTagSystem.Domain/Entity/Match.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using laserTagSystem.Domain.Enums;
 using MatchType = laserTagSystem.Domain.Enums.MatchType;
 
 namespace laserTagSystem.Domain.Entity;
 
 public class Match {
+    [Key]
     public Guid Id { get; set; }
     
     public DateTime StartingTime { get; set; }
@@ -15,4 +18,6 @@ public class Match {
     public MatchStatus Status { get; set; } // Scheduled, Ongoing, Finished
 
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public Field Field { get; set; } = null!;
 }
