cea4152f8ab073bb5b6c65857bea939d2cb5ebde
TalalBalnoob <TalalBalnoob@gmail.com>
2025-12-18 11:38:47 +0300

    feat: complet the Field repository

--- src/laserTagSystem.Application/Interfaces/IRepository/IFieldRepository.cs
+++ src/laserTagSystem.Application/Interfaces/IRepository/IFieldRepository.cs
@@ -4,5 +4,10 @@ namespace laserTagSystem.Application.Interfaces.IRepository;
 
 public interface IFieldRepository {
     Task<Field> AddAsync(Field field);
+    Task<Field> UpdateAsync(Field field);
+    void DeleteAsync(Field field);
     Task<List<Field>> GetAllAsync();
+    Task<Field?> GetByIdAsync(Guid id);
+    Task<List<Field>> GetAvailableAsync();
+    Task<List<Field>> GetUnBookedFieldsByDateAsync(DateOnly date);
 }
--- src/laserTagSystem.Infrastructure/Repository/FieldRepository.cs
+++ src/laserTagSystem.Infrastructure/Repository/FieldRepository.cs
@@ -2,6 +2,7 @@ using laserTagSystem.Application.Interfaces.IRepository;
 using laserTagSystem.Domain.Entity;
 using laserTagSystem.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace laserTagSystem.Infrastructure.Repository;
 
@@ -13,7 +14,42 @@ public class FieldRepository (AppDBContext _db): IFieldRepository {
         return field;
     }
 
+    public async Task<Field> UpdateAsync(Field field) {
+        var updatedField = _db.Fields.Update(field).Entity;
+        await _db.SaveChangesAsync();
+        return updatedField;
+
+    }
+
+    public async void DeleteAsync(Field field) {
+        _db.Fields.Remove(field);
+        await _db.SaveChangesAsync();
+    }
+
     public async Task<List<Field>> GetAllAsync() {
         return await _db.Fields.ToListAsync();
+    }
+
+    public async Task<Field?> GetByIdAsync(Guid id) {
+        return await _db.Fields.FindAsync(id);
+    }
+
+    public async Task<List<Field>> GetAvailableAsync() {
+        return await _db.Fields.Where(f => f.IsAvailable == true).ToListAsync();
+    }
+
+    public async Task<List<Field>> GetUnBookedFieldsByDateAsync(DateOnly date) {
+        var start = date.ToDateTime(TimeOnly.MinValue);
+        var end   = date.ToDateTime(TimeOnly.MaxValue);
+
+        return await _db.Fields
+            .Include(f => f.Matches)
+            .Where(field =>
+                !field.Matches.Any(match =>
+                    match.StartingTime < end &&
+                    match.EndingTime > start
+                )
+            )
+            .ToListAsync();
     }
 }
