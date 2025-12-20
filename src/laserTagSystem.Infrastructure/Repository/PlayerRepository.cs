8ea4e66ca36e9db0d26eb39189bb39c25b52824e
TalalBalnoob <TalalBalnoob@gmail.com>
2025-12-20 23:22:16 +0300

    feat: Player Repository and interface

--- src/laserTagSystem.Infrastructure/Repository/PlayerRepository.cs
+++ src/laserTagSystem.Infrastructure/Repository/PlayerRepository.cs
@@ -7,13 +7,31 @@ namespace laserTagSystem.Infrastructure.Repository;
 
 public class PlayerRepository (AppDBContext _db): IPlayerRepository {
     
+    public async Task<List<Player>> GetAllAsync() {
+        return await _db.Players.ToListAsync();
+    }
+    
     public async Task<Player> AddAsync(Player player) {
         _db.Players.Add(player);
         await _db.SaveChangesAsync();
         return player;
     }
 
-    public async Task<List<Player>> GetAllAsync() {
-        return await _db.Players.ToListAsync();
+    public async Task<Player> UpdateAsync(Player player) {
+        var Player = _db.Players.Update(player).Entity;
+        await _db.SaveChangesAsync();
+        return Player;
+
+    }
+
+    public void DeleteAsync(Player player) {
+        _db.Players.Remove(player);
+        _db.SaveChangesAsync();
+    }
+
+    
+
+    public async Task<Player?> GetByIdAsync(Guid id) {
+        return await _db.Players.FindAsync(id);
     }
 }
--- src/laserTagSystem.Infrastructure/Repository/PlayerScoreRepository.cs
+++ src/laserTagSystem.Infrastructure/Repository/PlayerScoreRepository.cs
@@ -16,4 +16,20 @@ public class PlayerScoreRepository (AppDBContext _db): IPlayerScoreRepository {
     public async Task<List<PlayerScore>> GetAllAsync() {
         return await _db.PlayerScores.ToListAsync();
     }
+    
+    public async Task<PlayerScore> UpdateAsync(PlayerScore playerScore) {
+        var PlayerScore = _db.PlayerScores.Update(playerScore).Entity;
+        await _db.SaveChangesAsync();
+        return PlayerScore;
+
+    }
+
+    public void DeleteAsync(PlayerScore playerScore) {
+        _db.PlayerScores.Remove(playerScore);
+        _db.SaveChangesAsync();
+    }
+
+    public async Task<PlayerScore?> GetByIdAsync(Guid id) {
+        return await _db.PlayerScores.FindAsync(id);
+    }
 }
