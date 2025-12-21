01ac588ff199ecb59cd6967fa3ff31303e5129e0
TalalBalnoob <TalalBalnoob@gmail.com>
2025-12-21 10:36:18 +0300

    feat: add new markUnavailable method to the Field model

--- src/laserTagSystem.Domain/Entity/Field.cs
+++ src/laserTagSystem.Domain/Entity/Field.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using laserTagSystem.Domain.Exceptions;
 
 namespace laserTagSystem.Domain.Entity;
 
@@ -14,4 +15,11 @@ public class Field {
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
     public ICollection<Match> Matches { get; set; } = new List<Match>();
+    
+    public void MarkUnavailable() {
+        if (!IsAvailable)
+            throw new DomainException("Field is already unavailable");
+    
+        IsAvailable = false;
+    }
 }
