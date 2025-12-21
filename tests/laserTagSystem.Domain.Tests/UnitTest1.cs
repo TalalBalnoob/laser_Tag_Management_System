5ddc9d04bb1afa3c21d16668cd7039157ae5bfef
TalalBalnoob <TalalBalnoob@gmail.com>
2025-12-21 11:12:33 +0300

    feat: add new test project for the domain project

---
+++ tests/laserTagSystem.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using laserTagSystem.Domain.Entity;
+using laserTagSystem.Domain.Enums;
+using laserTagSystem.Domain.Exceptions;
+using laserTagSystem.Domain.ValueObjects;
+using NUnit;
+
+namespace laserTagSystem.Domain.Tests;
+
+public class Tests {
+    // [SetUp]
+    // public void Setup() {
+    // }
+    //
+    // [Test]
+    // public void Test1() {
+    //     Assert.Pass();
+    // }
+    
+    [Test]
+    public void MatchTime_EndBeforeStart_ShouldThrow()
+    {
+        var start = DateTime.UtcNow;
+        var end = start.AddMinutes(-30);
+
+        Assert.Throws<DomainException>(() =>
+            new MatchTime(start, end));
+    }
+
+    [Test]
+    public void Match_State_Change() {
+        var match = new Match();
+
+        match.MarkScheduled();
+        Assert.Equals(MatchStatus.Scheduled, match.Status);
+    }
+
+}
